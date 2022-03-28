34700a37de62d2cf3f4b8a495ac0cf47f6abc627
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-28 19:29:22 +0200

    Display courses

--- src/pages/user/Feed.jsx
+++ src/pages/user/Feed.jsx
@@ -1,11 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import {
+	Timestamp,
+	collection,
+	orderBy,
+	query,
+	where,
+} from "firebase/firestore";
 
 import { StatusBar } from "expo-status-bar";
+import { db } from "../../../firebase";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 const Feed = () => {
+	const [courses, loading, error] = useCollectionData(
+		query(collection(db, "courses"))
+	);
+
+	const renderCourse = ({ item }) => (
+		<View style={styles.course}>
+			<Text style={styles.title}>{item.title}</Text>
+			<Text style={styles.instructor}>{item.instructor}</Text>
+
+			<Text style={styles.date}>{item.startDate.toDate().toString()}</Text>
+			<Text style={styles.date}>{item.endDate.toDate().toString()}</Text>
+
+			<Text style={styles.platform}>{item.platform}</Text>
+			<Text style={styles.link}>{item.link}</Text>
+		</View>
+	);
+
 	return (
 		<View style={styles.container}>
-			<Text>Feed</Text>
+			{courses && (
+				<FlatList
+					data={courses}
+					renderItem={renderCourse}
+					keyExtractor={(item, index) => index}
+				/>
+			)}
 
 			<StatusBar style="auto" />
 		</View>
