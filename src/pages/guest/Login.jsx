66fbe947b66b6207026798de49ab31c5c36d2fd5
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-28 18:40:41 +0200

    Implement user login

--- src/pages/guest/Login.jsx
+++ src/pages/guest/Login.jsx
@@ -1,3 +1,62 @@
-const Login = () => <></>;
+import {
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from "react-native";
+
+import { StatusBar } from "expo-status-bar";
+import { auth } from "../../../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+
+const Login = () => {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleSubmit = async () => {
+		await signInWithEmailAndPassword(auth, username, password);
+	};
+
+	return (
+		<View style={styles.container}>
+			<TextInput
+				style={styles.input}
+				placeholder="Username"
+				onChangeText={setUsername}
+			/>
+			<TextInput
+				style={styles.input}
+				placeholder="Password"
+				onChangeText={setPassword}
+				secureTextEntry={true}
+			/>
+
+			<TouchableOpacity onPress={() => handleSubmit()}>
+				<Text style={styles.button}>Login</Text>
+			</TouchableOpacity>
+
+			<StatusBar style="auto" />
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "white",
+	},
+
+	input: {
+		height: 50,
+	},
+
+	button: {
+		marginTop: 10,
+	},
+});
 
 export default Login;
