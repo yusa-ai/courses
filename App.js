b7fd3db59b575efb497d8cc3003b1f211d8f7cd6
Ryan Malonzo <ryan@ryanmalonzo.fr>
2022-03-29 10:40:31 +0200

    Ignore annoying warning on Android

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import { LogBox } from "react-native";
 import Routes from "./src/routes/Routes";
 import TimeAgo from "javascript-time-ago";
 import { auth } from "./firebase";
 import en from "javascript-time-ago/locale/en.json";
 import fr from "javascript-time-ago/locale/fr.json";
 import { useAuthState } from "react-firebase-hooks/auth";
+
+LogBox.ignoreLogs(["Setting a timer"]); // Ignore annoying warning on Android
 
 TimeAgo.addDefaultLocale(en);
 TimeAgo.addLocale(fr);
